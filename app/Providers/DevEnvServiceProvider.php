4e1fa0bd017a8eb3e9349981debb30673b02ebe2
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2017-07-28 12:33:24 +0200

    Add new Service provider specific for development

---
+++ app/Providers/DevEnvServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Wdi\Providers;
+
+use Barryvdh\Debugbar\Facade as DebugbarFacade;
+use Barryvdh\Debugbar\ServiceProvider as DebugbarServiceProvider;
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
+use Faker\Factory as FakerFactory;
+use Faker\Generator as FakerGenerator;
+use Illuminate\Support\ServiceProvider;
+
+final class DevEnvServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+    
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if (in_array($this->app->environment(), ["local", "testing", "staging", "development", "dev"])) {
+            $this->app->register(DebugbarServiceProvider::class);
+            $this->app->register(IdeHelperServiceProvider::class);
+            $this->app->alias("Debugbar", DebugbarFacade::class);
+            
+            $this->app->singleton(FakerGenerator::class, function () {
+                return FakerFactory::create(config("app.locale_extended"));
+            });
+        }
+    }
+}
