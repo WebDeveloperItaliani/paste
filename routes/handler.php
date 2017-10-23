c1e8131cba522ad6b5a973603ad5d1ae5545c867
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2017-10-23 17:19:19 +0200

    Add show paste route

--- routes/handler.php
+++ routes/handler.php
@@ -1,7 +1,12 @@
 <?php
 
+use Wdi\Http\Handlers\Paste\{
+    AddPasteHandler, CreatePasteHandler, ShowPasteHandler
+};
 
-Route::prefix("pastes")->group(function() {
-    Route::get("new", \Wdi\Http\Handlers\CreatePasteHandler::class);
-    Route::post("", \Wdi\Http\Handlers\AddPasteHandler::class);
+Route::prefix("pastes")->group(function () {
+    Route::get("new", CreatePasteHandler::class);
+    Route::post("", AddPasteHandler::class);
+    
+    Route::get("{paste}", ShowPasteHandler::class);
 });
