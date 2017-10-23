ac98d51524fbaa7d1d030aa0d59744ef13fbf103
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2017-10-23 17:45:13 +0200

    Add new paste observer for auto generating slug

---
+++ app/Observers/PasteObserver.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Wdi\Observers;
+
+use Wdi\Entities\Paste;
+
+/**
+ * Class PasteObserver
+ * @package Wdi\Observers
+ */
+final class PasteObserver
+{
+    /**
+     * When a paste is being created a
+     * new slug is generated which is unique
+     *
+     * @param \Wdi\Entities\Paste $paste
+     */
+    public function creating(Paste $paste)
+    {
+        $paste->slug = str_random();
+    }
+    
+}
