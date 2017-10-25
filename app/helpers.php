244cd49d35e300c58a2e575c286078583b5e1001
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2017-10-25 11:58:06 +0200

    Improve slugfy algorithm

--- app/helpers.php
+++ app/helpers.php
@@ -3,28 +3,20 @@
 
 if (!function_exists("slugfy")) {
     /**
-     * Procedural generate slug
+     * Procedural generated slug
      *
      * @param int $length
      * @return string
      */
     function slugfy(int $length = 3) : string
     {
-        $combination = "";
+        $basket = array_merge(config("procedural.adjectives"), config("procedural.animals"));
         
-        for ($i = 0; $i < $length; $i++) {
-            
-            $seed = array_random(config("procedural.animals"));
-            
-            if (mt_rand() % 2 === 0) {
-                $seed = array_random(config("procedural.adjectives"));
-            }
-            
-            if (!str_contains($combination, $seed)) {
-                $combination .= ucfirst($seed);
-            }
-        }
+        $combination = array_map(
+            "ucfirst",
+            array_random($basket, $length)
+        );
         
-        return $combination;
+        return implode("", $combination);
     }
 }
