666d96c7bb753bdde6dbd746440efee57dea773d
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2017-10-26 18:51:33 +0200

    Improve navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,26 +1,15 @@
 <nav class="navbar navbar-expand navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Bottom navbar</a>
+    <a class="navbar-brand" href="#">WDI Paste</a>
     <button class="navbar-toggler" aria-expanded="false" aria-controls="navbarCollapse" aria-label="Toggle navigation" type="button" data-toggle="collapse" data-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route("home") }}">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-            <li class="nav-item dropup">
-                <a class="nav-link dropdown-toggle" id="dropdown10" aria-expanded="false" aria-haspopup="true" href="https://getbootstrap.com" data-toggle="dropdown">Dropup</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown10">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
+                <a class="nav-link" href="{{ route("language.list") }}">Linguaggi</a>
             </li>
         </ul>
     </div>
