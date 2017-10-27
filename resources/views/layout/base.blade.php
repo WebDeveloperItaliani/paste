552e003740404ee250059e2b05e662ab6a833872
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2017-10-27 11:15:07 +0200

    Improve base layout

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -4,12 +4,17 @@
         @include("layout.header")
         <title>@yield("title")</title>
         @include("layout.css")
+        @yield("css")
     </head>
 
     <body>
         @include("components.navbar")
+        @include("components.flash-message")
         <div class="container">
             @yield("container")
         </div>
+
+        @include("layout.js")
+        @yield("js")
     </body>
 </html>
