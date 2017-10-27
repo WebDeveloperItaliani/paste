10e93ce7b652cc2f5ecc7805e9a9ff78589529c7
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2017-10-27 11:20:02 +0200

    Improve navbar navigation

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,13 +1,10 @@
 <nav class="navbar navbar-expand navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">WDI Paste</a>
+    <a class="navbar-brand" href="{{ route("home") }}">WDI Paste</a>
     <button class="navbar-toggler" aria-expanded="false" aria-controls="navbarCollapse" aria-label="Toggle navigation" type="button" data-toggle="collapse" data-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route("home") }}">Home</a>
-            </li>
             <li class="nav-item">
                 <a class="nav-link" href="{{ route("language.list") }}">Linguaggi</a>
             </li>
