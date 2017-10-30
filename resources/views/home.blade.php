ce25c6360b64f67f362081e1a00dfc8ace3d97ea
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2017-10-30 16:45:29 +0100

    Add fancy button to home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,9 @@
         <div class="col  text-center">
             <h1 class="display-1">WDI Paste</h1>
             <p class="lead">WDI Paste ti permette di creare e condividere Paste</p>
-            <a class="btn btn-lg btn-primary" href="{{ route("paste.create") }}">Crea nuovo Paste</a>
+
+            <a class="btn btn-lg btn-outline-info" href="{{ route("language.list") }}">Scopri i linguaggi</a>
+            <a class="btn btn-lg btn-outline-primary" href="{{ route("paste.create") }}">Crea nuovo Paste</a>
         </div>
     </main>
 @endsection
