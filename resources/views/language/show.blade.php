e4859f7d34a57791bd14996a892d5973c0b08633
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2017-10-31 16:49:37 +0100

    Improve language show view UX

--- resources/views/language/show.blade.php
+++ resources/views/language/show.blade.php
@@ -10,24 +10,27 @@
         <span class="text-muted">{{ $language->extensionsToString }}</span>
     </p>
 
-    <div class="card-columns">
-        @forelse($language->pastes as $paste)
-            <div class="card">
-                <div class="card-body">
-                    <h4 class="card-title text-capitalize">{{ $paste->fileName }}</h4>
-                    <p class="card-text">{{ $paste->description }}</p>
-                    <a href="{{ route("paste.show", $paste->slug) }}" class="card-link">Visualizza</a>
+    @if($language->hasPastes())
+        <div class="card-columns">
+            @foreach($language->pastes as $paste)
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title text-capitalize">{{ $paste->fileName }}</h4>
+                        <p class="card-text">{{ $paste->description }}</p>
+                        <a href="{{ route("paste.show", $paste->slug) }}" class="card-link">Visualizza</a>
+                    </div>
                 </div>
+            @endforeach
+        </div>
+    @else
+        <div class="card border-secondary">
+            <div class="card-header">Opps nessuno paste per {{ $language->name }}</div>
+            <div class="card-body text-secondary">
+                <h4 class="card-title">{{ $language->name }} è senza paste</h4>
+                <p class="card-text">Questo è dovuto al fatto che questo linguaggio non è stato ancora utilizzato per creare dei paste, vuoi essere il primo?</p>
+                <a href="{{ route("paste.create") }}">Crea il primo paste per {{ $language->name }}</a>
             </div>
-        @empty
-            <div class="card border-secondary">
-                <div class="card-header">Opps nessuno paste per {{ $language->name }}</div>
-                <div class="card-body text-secondary">
-                    <h4 class="card-title">{{ $language->name }} è senza paste</h4>
-                    <p class="card-text">Questo è dovuto al fatto che questo linguaggio non è stato ancora utilizzato per creare dei paste, vuoi essere il primo?</p>
-                    <a href="{{ route("paste.create") }}">Crea il primo paste per {{ $language->name }}</a>
-                </div>
-            </div>
-        @endforelse
-    </div>
+        </div>
+    @endif
+
 @endsection
