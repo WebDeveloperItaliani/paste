0dbf99d9ec5544a176d65c85f38106bbcb6b56f5
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2017-10-31 11:40:45 +0100

    Improve LanguageSelect vue component

--- resources/assets/js/components/LanguageSelect.vue
+++ resources/assets/js/components/LanguageSelect.vue
@@ -2,18 +2,18 @@
     <div class="form-row col-md-6">
         <div class="form-group col-md-6">
             <label for="paste-language">Linguaggio</label>
-            <select name="language_id" id="paste-language" class="form-control" @change="setLanguage">
+            <select name="language_id" id="paste-language" class="form-control" @change="changeLanguage">
                 <option selected aria-selected="true" disabled>Seleziona un linguaggio</option>
-                <option v-for="language in languages" :value="language.id">{{ language.name}}</option>
+                <option v-for="lang in languages" :value="lang.id" :selected="isSelectedLanguage(lang)">{{ lang.name}} </option>
             </select>
         </div>
 
         <div class="form-group col-md-6">
             <label for="paste-extension">Estensione</label>
-            <select name="extension" id="paste-extension" class="form-control" :disabled="language == null">
+            <select name="extension" id="paste-extension" class="form-control" :disabled="!isLanguageSelected">
                 <option selected aria-selected="true" disabled>Seleziona un'estensione</option>
-                <template v-if="language != null">
-                    <option v-for="name in language.extensions" :value="name">{{ name }}</option>
+                <template v-if="isLanguageSelected">
+                    <option v-for="name in this.selectedLanguage.extensions" :value="name" :selected="isSelectedExtension(name)">{{ name }} </option>
                 </template>
             </select>
         </div>
@@ -22,33 +22,64 @@
 
 <script>
     const {
-        Axios: { get },
-        _: { find }
+        Axios: {get},
+        _: {find},
     } = window;
 
     export default {
-        name: "language-select",
+        name: 'language-select',
+
+        props: {
+            language: {
+                type: Object,
+                required: false,
+                default: () => null,
+            },
+            extension: {
+                type: String,
+                required:false,
+                default: () => '',
+            },
+        },
 
         data() {
             return {
                 languages: [],
-                language: null,
-            }
+                selectedLanguage: null,
+            };
         },
 
         mounted() {
-            get("/api/languages")
-                .then(({data} = response) => {
-                    this.languages = data.languages;
-                });
+            get('/api/languages')
+                .then(({data} = response) => this.languages = data.languages);
+
+            this.selectedLanguage = this.language;
         },
 
         methods: {
-            setLanguage(e) {
-                this.language = find(this.languages, (lang) => {
-                    return lang.id - e.target.value === 0;
+            isSelectedLanguage(lang) {
+                return (this.isLanguageSelected) ? lang.id === this.selectedLanguage.id : false;
+            },
+
+            isSelectedExtension(name) {
+                return name === this.extension;
+            },
+
+            changeLanguage(e) {
+                this.setLanguage(e.target.value);
+            },
+
+            setLanguage(id) {
+                this.selectedLanguage = find(this.languages, (lang) => {
+                    return lang.id - id === 0;
                 });
             }
+        },
+
+        computed: {
+            isLanguageSelected() {
+                return this.selectedLanguage !== null;
+            }
         }
-    }
+    };
 </script>
