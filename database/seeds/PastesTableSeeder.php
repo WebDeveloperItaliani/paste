6e5ad23dae5c9b9c008dbea569368b64f084417b
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2017-11-07 09:04:23 +0100

    Add fixed Paste test on PasteTableSeeder

--- database/seeds/PastesTableSeeder.php
+++ database/seeds/PastesTableSeeder.php
@@ -14,13 +14,20 @@ final class PastesTableSeeder extends Seeder
      */
     public function run()
     {
+        // Create a password fixed test
+        factory(\Wdi\Entities\Paste::class)->states("with-password")->create([
+            "language_id" => \Wdi\Entities\Language::first()->id,
+            "name" => "DatabaseFactory",
+            "password" => "foobar",
+        ]);
+        
         // Create a bunch of anon pastes
-        factory(\Wdi\Entities\Paste::class, 100)->create([
+        factory(\Wdi\Entities\Paste::class, 100)->states("with-password")->create([
             "language_id" => \Wdi\Entities\Language::first()->id,
         ]);
-
+        
         // Create forks of a single paste
-        factory(\Wdi\Entities\Paste::class, 10)->create([
+        factory(\Wdi\Entities\Paste::class, 10)->states("with-password")->create([
             "paste_id" => \Wdi\Entities\Paste::first()->id,
         ]);
     }
