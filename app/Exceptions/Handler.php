6c8cfd2b23a6ca01e417456b2991b9ffbc0a443a
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2017-11-07 09:15:25 +0100

    Remove dead code

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -43,14 +43,4 @@ class Handler extends ExceptionHandler
     {
         return parent::render($request, $exception);
     }
-    
-    /** {@inheritdoc} */
-    protected function unauthenticated($request, AuthenticationException $exception)
-    {
-        if ($request->expectsJson()) {
-            return response()->json(["error" => "Unauthenticated."], 401);
-        }
-        
-        return redirect()->guest(route("login"));
-    }
 }
