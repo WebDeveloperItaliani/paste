2c4c1ac3ad3dd027edcb5e463c1ecb583f824985
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2017-11-09 11:35:08 +0100

    Add Laravel socialite and draft a config system

--- config/services.php
+++ config/services.php
@@ -13,26 +13,11 @@ return [
     | to have a conventional place to find your various credentials.
     |
     */
-
-    "mailgun" => [
-        "domain" => env("MAILGUN_DOMAIN"),
-        "secret" => env("MAILGUN_SECRET"),
-    ],
-
-    "ses" => [
-        "key" => env("SES_KEY"),
-        "secret" => env("SES_SECRET"),
-        "region" => "us-east-1",
-    ],
-
-    "sparkpost" => [
-        "secret" => env("SPARKPOST_SECRET"),
-    ],
-
-    "stripe" => [
-        "model" => Wdi\User::class,
-        "key" => env("STRIPE_KEY"),
-        "secret" => env("STRIPE_SECRET"),
+    
+    "facebook" => [
+        'client_id' => env("FB_CLIENT_ID"),
+        'client_secret' => env("FB_CLIENT_SECRET"),
+        'redirect' => env("FB_CALLBACK_URL"),
     ],
 
 ];
